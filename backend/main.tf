5ffc4129a3968b0df02032814804dc62ad65464b
Sreyas Tendulkar <tendulkarsreyas1415@gmail.com>
2026-08-22 02:20:21 +0530

    Remove the outdated dynamodb statefile locking and implement s3 native statefile locking

--- backend/main.tf
+++ backend/main.tf
@@ -21,15 +21,23 @@ resource "aws_s3_bucket" "my-statefile-bucket_devopsify" {
   }
 }
 
-resource "aws_dynamodb_table" "staefile_locking-table" {
-  name           = "devopsify_state_file"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockId"
 
-   attribute {
-    name = "LockId"
-    type = "S"
+terraform {
+  backend "s3" {
+    bucket       = "my-statefile-bucket-devopsify"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
-
-
 }
+
+resource "aws_s3_bucket" "my-statefile-bucket_devopsify-prod" {
+  bucket = "my-statefile-bucket-devopsify-prod"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "prod"
+  }
+}
+
